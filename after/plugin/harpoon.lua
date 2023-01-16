076dcb6269bbc434454a00c544ccc19f02d8e5a6
Sebastian Finkel <s.finkel@posteo.de>
2023-01-16 13:10:39 +0100

    add keybindings for harpoon

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,8 +1,22 @@
 
-local opts = {noremap = true, silent = true}
+local opts = {noremap = true, silent = true, expr = false}
 
 local keymap = vim.api.nvim_set_keymap
+-- require("harpoon").setup({ ... })
+local found, harpoon_mark = pcall(require, 'harpoon.mark')
+if not found then
+  return
+end
 
-local found, harpoon_marks = pcall(require, 'harpoon.marks')
+local ui_found, ui = pcall(require, 'harpoon.ui')
+if not ui_found then
+  return
+end
 
--- keymap("n", "<Leader>ha", "harpoon_marks")
+keymap("n", "<leader>ha", '<Cmd>lua require("harpoon.mark").add_file()<CR>', opts)
+keymap('n', '<leader>hh',  '<Cmd>lua require("harpoon.ui").toggle_quick_menu()<CR>', opts)
+
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
